ca9e06a462876453397d7871bde90c2f9ad5c51e
AxelLabadie <axel.labadie@limayrac.fr>
2022-06-30 17:21:51 +0200

    ajout stream

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+
+    
   </ion-app>
 </template>
 
@@ -8,11 +10,35 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+
+
 export default defineComponent({
   name: 'App',
+   data() {
+    return {
+      currentAudioName: '',
+      audioList: [
+        {
+          name: 'audio1',
+          url: 'https://stream-ssl.radios-arra.fr:8443/gascognefm'
+        }
+      ]
+    }
+  },
   components: {
     IonApp,
-    IonRouterOutlet
+    IonRouterOutlet,
+    AudioPlayer
+  },
+   methods: {
+    // Something to do before playing
+    handleBeforePlay(next) {
+      // There are a few things you can do here...
+      this.currentAudioName = this.audioList[this.$refs.audioPlayer.currentPlayIndex].name
+
+      next() // Start playing
+    }
   }
+
 });
 </script>
